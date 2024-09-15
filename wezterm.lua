41e7774dfec072b8505b44cc3d52c25e4790f3af
Prerit Dayal <62067658+prerit714@users.noreply.github.com>
2024-09-15 04:51:20 -0400

    New wezterm config to simulate tmux

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local act = wezterm.action
 
--- Open wezterm in maximized mode by default
+act({ SpawnCommandInNewTab = { cwd = wezterm.home_dir } })
+
 wezterm.on("gui-startup", function()
 	local _, _, window = mux.spawn_window({})
 	window:gui_window():maximize()
@@ -13,12 +15,15 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- Font config
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "Gruvbox dark, hard (base16)"
+
 config.font = wezterm.font("IosevkaTerm NFM", {
 	stretch = "Expanded",
 	weight = "Bold",
 })
+config.default_prog = {
+	"ubuntu",
+}
 config.font_size = 24
 
 -- Config for padding
@@ -29,12 +34,65 @@ config.window_padding = {
 	bottom = 5,
 }
 
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
 -- Hide native tabs since I use tmux
 config.enable_tab_bar = false
 
--- Launching tmux at start
-config.default_prog = {
-	"tmux",
+-- Leader is the same as my old tmux prefix
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "=",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- Navigation
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	-- open tab in the current domain
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
 }
+
+for i = 0, 9 do
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
 
 return config
